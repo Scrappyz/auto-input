dee077bea88bf62e46f11ba5beb7b25a66368a5b
Scrappyz <ghostmic3000@gmail.com>
2023-12-20 11:05:45 +0800

    added bidict for mouse buttons

--- project/autoinput.py
+++ project/autoinput.py
@@ -25,6 +25,8 @@ __keys = __bidict({'""': 222, '*': 106, '+': 107, ',': 188, '-': 109, '.': 190, 
     "tab": 9, "up": 38, '[': 219, '\\\\': 220, ']': 221, '`': 192, 'a': 65, 'b': 66, 'c': 67, 'd': 68, 'e': 69, 
     'f': 70, 'g': 71, 'h': 72, 'i': 73, 'j': 74, 'k': 75, 'l': 76, 'm': 77, 'n': 78, 'o': 79, 'p': 80, 'q': 81, 'r': 82, 
     's': 83, 't': 84, 'u': 85, 'v': 86, 'w': 87, 'x': 88, 'y': 89, 'z': 90})
+__buttons = __bidict({"LMB": __mouse.Button.left, "MMD": __mouse.Button.middle, "RMB": __mouse.Button.right, 
+                      "X1": __mouse.Button.x1, "X2": __mouse.Button.x2})
 
 def typeVal(v):
     print("{0} | {1}".format(type(v), v))
